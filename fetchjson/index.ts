995778e5e252ca4bd1c90ed9638dcfd4bf97e509
matijaskorup1991 <smatija1991@gmail.com>
2021-06-15 11:39:45 +0200

    test1

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -10,13 +10,17 @@ interface Todo {
 
 axios.get(url).then((res) => {
   const data = res.data as Todo;
-  const ID = data.ID;
-  const title = data.Title;
-  const finished = data.finished;
+  const ID = data.id;
+  const title = data.title;
+  const finished = data.completed;
 
+  logTodo(ID, title, finished);
+});
+
+const logTodo = (id: number, title: string, finished: boolean) => {
   console.log(`
-  the todo with id: ${ID} 
+  the todo with id: ${id} 
   title: ${title} 
   finished: ${finished}
   `);
-});
+};
