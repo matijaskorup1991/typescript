e195f38c058189f6c6b674fff0bc846ce88f22e2
matijaskorup1991 <smatija1991@gmail.com>
2021-06-16 12:41:30 +0200

    test

---
+++ test/app.js
@@ -0,0 +1,6 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var a = 5;
+var b = 10;
+console.log(add(5, 10));
---
+++ test/app.ts
@@ -0,0 +1,13 @@
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+let a = 5;
+let b = 10;
+
+console.log(add(5, 10));
+
+let person = {
+  name: 'Matija',
+  age: 30,
+};
