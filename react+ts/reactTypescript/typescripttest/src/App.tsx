2449614f6e805e5def2f684bcc4712c92d53e96a
matijaskorup1991 <smatija1991@gmail.com>
2021-06-16 16:42:52 +0200

    state

--- react+ts/reactTypescript/typescripttest/src/App.tsx
+++ react+ts/reactTypescript/typescripttest/src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todo from './components/Todo';
 import NewTodo from './components/NewTodo';
 
+interface Todo {
+  id: string;
+  text: string;
+}
+
 const App: React.FC = () => {
-  const todos = [
+  const [todos, setTodos] = useState<Todo[]>([
     { id: 't1', text: 'lorem' },
     { id: 't2', text: 'ipsum' },
-  ];
+  ]);
 
   const todoAddHandler = (text: string) => {
+    setTodos((prevState) => [
+      ...prevState,
+      { id: Math.random().toString(), text },
+    ]);
     console.log(text);
   };
 
