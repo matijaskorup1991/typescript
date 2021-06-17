b8d3aaaa4e922a43931e17a190b6688b76de65a0
matijaskorup1991 <smatija1991@gmail.com>
2021-06-17 15:17:54 +0200

    vowels

--- algorithms/index.js
+++ algorithms/index.js
@@ -6,4 +6,29 @@ function convertNames(str) {
         .join('')
         .toUpperCase();
 }
-console.log(convertNames('Matija Skorup'));
+// console.log(convertNames('matija Skorup'));
+function sumPos(arr) {
+    return arr.filter(function (el) { return el > 0; }).reduce(function (a, b) { return a + b; });
+}
+// console.log(sumPos([1, 2, 3, -10, -12]));
+function matchPoints(arr) {
+    var num = 0;
+    var myArr = arr
+        .map(function (el) { return el.split(':'); })
+        .map(function (el) {
+        return el[0] > el[1] ? (num += 3) : el[0] == el[1] ? (num += 1) : (num += 0);
+    });
+    return num;
+}
+// console.log(matchPoints(['3:1', '3:1', '2:1', '1:1']));
+function vowelThere(arr) {
+    var vowels = 'aeiou';
+    //   let maArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (vowels.indexOf(String.fromCharCode(arr[i])) >= 0) {
+            arr[i] = String.fromCharCode(arr[i]);
+        }
+    }
+    return arr;
+}
+// console.log(vowelThere([101, 121, 110]));
--- algorithms/index.ts
+++ algorithms/index.ts
@@ -6,4 +6,35 @@ function convertNames(str: string): string {
     .toUpperCase();
 }
 
-console.log(convertNames('Matija Skorup'));
+// console.log(convertNames('matija Skorup'));
+
+function sumPos(arr: number[]): number {
+  return arr.filter((el) => el > 0).reduce((a, b) => a + b);
+}
+
+// console.log(sumPos([1, 2, 3, -10, -12]));
+
+function matchPoints(arr: string[]): number {
+  let num = 0;
+  let myArr = arr
+    .map((el) => el.split(':'))
+    .map((el) =>
+      el[0] > el[1] ? (num += 3) : el[0] == el[1] ? (num += 1) : (num += 0)
+    );
+  return num;
+}
+
+// console.log(matchPoints(['3:1', '3:1', '2:1', '1:1']));
+
+function vowelThere(arr: number[]): number[] | string[] {
+  let vowels = 'aeiou';
+  //   let maArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (vowels.indexOf(String.fromCharCode(arr[i])) >= 0) {
+      arr[i] = String.fromCharCode(arr[i]);
+    }
+  }
+  return arr;
+}
+
+// console.log(vowelThere([101, 121, 110]));
