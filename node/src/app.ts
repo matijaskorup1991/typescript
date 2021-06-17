92c1868585b70b1851103ab622f3880312c35e1a
matijaskorup1991 <smatija1991@gmail.com>
2021-06-17 07:16:58 +0200

    routes

---
+++ node/src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+const app = express();
+
+import todoRoutes from './routes/todos';
+
+app.use('/todos', todoRoutes);
+
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    res.status(500).json({ message: err.message });
+  }
+);
+
+app.listen(1235);
